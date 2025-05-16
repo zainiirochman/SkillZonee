a5f20d0df8d16b8afc2b0d4ca1f046c7060477f4
zainiirochman <rochman279@gmail.com>
2025-05-16 11:26:57 +0700

    update upload soal

--- app/src/main/java/com/example/skillzonee/model/MateriModel.java
+++ app/src/main/java/com/example/skillzonee/model/MateriModel.java
@@ -1,71 +1,29 @@
 package com.example.skillzonee.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class MateriModel {
-    private String id; // Quiz ID
-    private String title; // Quiz title
-    private String subtitle; // Quiz subtitle
-    private String time; // Quiz duration as a string
-    private List<QuestionModel> questionList; // List of questions in the quiz
+    private String title;
+    private String description;
+    private String youtubeLink;
 
-    // Constructor to create a new QuizModel object
-    public MateriModel(String id, String title, String subtitle, String time, List<QuestionModel> questionList) {
-        this.id = id;
-        this.title = title;
-        this.subtitle = subtitle;
-        this.time = time;
-        this.questionList = questionList;
-    }
-
-    // Default constructor
     public MateriModel() {
-        this("", "", "", "", new ArrayList<>());
+        // Diperlukan oleh Firebase
     }
 
-    // Getter and setter for id
-    public String getId() {
-        return id;
+    public MateriModel(String title, String description, String youtubeLink) {
+        this.title = title;
+        this.description = description;
+        this.youtubeLink = youtubeLink;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    // Getter and setter for title
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public String getDescription() {
+        return description;
     }
 
-    // Getter and setter for subtitle
-    public String getSubtitle() {
-        return subtitle;
-    }
-
-    public void setSubtitle(String subtitle) {
-        this.subtitle = subtitle;
-    }
-
-    // Getter and setter for time
-    public String getTime() {
-        return time;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
-    }
-
-    // Getter and setter for questionList
-    public List<QuestionModel> getQuestionList() {
-        return questionList;
-    }
-
-    public void setQuestionList(List<QuestionModel> questionList) {
-        this.questionList = questionList;
+    public String getYoutubeLink() {
+        return youtubeLink;
     }
 }
